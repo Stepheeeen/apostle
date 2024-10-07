a5f78aed1c63014cffc5bf609f58169be87d61bb
stepheeeen <stephenon56@gmail.com>
2024-10-07 10:18:19 +0100

    october 7th

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import tailwind from 'twrnc'
+
+export default function Page() {
+  return (
+    <View style={tailwind`h-full w-full`}>
+
+    </View>
+  );
+}
