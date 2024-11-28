e6cbefa76984aebb577e968bd2876191a808a078
stepheeeen <stephenon56@gmail.com>
2024-11-29 00:18:19 +0100

    God please

---
+++ assets/images/icons.jsx
@@ -0,0 +1,14 @@
+export const LibraryIcon = () => (
+  <svg
+    width="30"
+    height="28"
+    viewBox="0 0 30 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.2064 0.237264L29.25 27.2847L27.8526 27.9099L15.8091 0.862433L17.2064 0.237264ZM0.75 27.8915V0.0901489H2.29457V27.8915H0.75ZM10.0171 27.8915V0.0901489H11.5617V27.8915H10.0171Z"
+      fill="white"
+    />
+  </svg>
+);
--- components/reusable/Navbar.jsx
+++ components/reusable/Navbar.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { View, Keyboard, TouchableOpacity, Text } from "react-native";
-import { router, usePathname } from 'expo-router';
+import {
+  View,
+  Keyboard,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { router, usePathname } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import Entypo from "@expo/vector-icons/Entypo";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import Svg, { Path } from "react-native-svg"; // Import Svg and Path
 import tw from "twrnc";
 
 const NavigationBar = () => {
@@ -11,11 +18,13 @@ const NavigationBar = () => {
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
 
   useEffect(() => {
-    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", () =>
-      setKeyboardVisible(true)
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => setKeyboardVisible(true)
     );
-    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () =>
-      setKeyboardVisible(false)
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => setKeyboardVisible(false)
     );
 
     return () => {
@@ -33,10 +42,10 @@ const NavigationBar = () => {
         <Entypo
           name="home"
           size={27}
-          color={pathname === "/Pages/Home" ? "#0081C9" : "black"}
+          color={pathname === "/Pages/Home" ? "#FFFFFF" : "#FFFFFF"}
         />
       ),
-      path: "/Pages/Home"
+      path: "/Pages/Home",
     },
     {
       link: "search",
@@ -44,34 +53,41 @@ const NavigationBar = () => {
         <AntDesign
           name="search1"
           size={27}
-          color={pathname === "/Pages/Search" ? "#0081C9" : "black"}
+          color={pathname === "/Pages/Search" ? "#FFFFFF" : "#FFFFFF"}
         />
       ),
-      path: "/Pages/Search"
+      path: "/Pages/Search",
     },
     {
       link: "library",
       icon: (
-        <MaterialIcons
-          name="library-music"
-          size={30}
-          color={pathname === "/Pages/Library" ? "#0081C9" : "black"}
-        />
+        <Svg
+          width="27"
+          height="27"
+          viewBox="0 0 30 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <Path
+            d="M17.2064 1.23726L29.25 28.2847L27.8526 28.9099L15.8091 1.86243L17.2064 1.23726ZM0.75 28.8915V1.09015H2.29457V28.8915H0.75ZM10.0171 28.8915V1.09015H11.5617V28.8915H10.0171Z"
+            fill="white"
+          />
+        </Svg>
       ),
-      path: "/Pages/Library"
+      path: "/Pages/Library",
     },
   ];
 
   return (
     <View
-      style={tw`absolute bottom-0 left-0 right-0 bg-white p-3 py-5 shadow-lg flex-row justify-between items-center`}
+      style={tw`absolute bottom-0 left-0 right-0 bg-[#0081C9] p-3 py-5 shadow-lg flex-row justify-between items-center`}
     >
       {Links.map((link, index) => (
         <TouchableOpacity
           key={index}
           style={tw`flex-row items-center px-5 py-1 ${
             pathname === link.path
-              ? "bg-[#0081C9]/20 border border-[#0081C9] rounded-full py-2"
+              ? "border border-white rounded-full py-2 gap-2"
               : ""
           }`}
           onPress={() => router.push(link.path)}
@@ -79,7 +95,7 @@ const NavigationBar = () => {
           {pathname === link.path ? (
             <View style={tw`flex flex-row gap-x-2`}>
               {link.icon}
-              <Text style={tw`font-medium text-lg text-[#0081C9]`}>
+              <Text style={tw`font-medium text-lg text-[#FFFFFF]`}>
                 {link.link.charAt(0).toUpperCase() + link.link.slice(1)}
               </Text>
             </View>
@@ -92,4 +108,4 @@ const NavigationBar = () => {
   );
 };
 
-export default NavigationBar;
+export default NavigationBar; 
